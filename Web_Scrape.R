a860a2644215288054fcef2f8891a73f1f805dea
Shaveen Britto <44424699+shaveen27@users.noreply.github.com>
2025-03-01 02:06:56 -0700

    Update2

--- Web_Scrape.R
+++ Web_Scrape.R
@@ -18,26 +18,26 @@ teams <- read_html(url) |>
 teams <- teams[!is.na(teams)]
 
 
-for (i in 1:length(teams)) {
+for (i in 29:length(teams)) {
   for (j in 1:length(years)) {
     tryCatch({
     url1 <- paste0("https://www.sports-reference.com/",teams[i],years[j],".html")
-    
+
     team <- unlist(str_split(unlist(str_split(teams[i], pattern = "schools/"))[2],"/"))[1]
-    
+
     html_content <- read_html(url1)
-    
-    # Passing 
+
+    # Passing
     passing_standard <- html_content |>
       html_node("#passing_standard") |>
       html_table(fill = TRUE) |>
-      filter(!is.na(Rk))|>  
+      filter(!is.na(Rk))|>
       mutate(Team = team, Year = years[j])
-    
+
     write.csv(passing_standard,
-              paste0("passing_standard/passing_standard","_",team,"_",years[j],".csv"), 
+              paste0("passing_standard/passing_standard","_",team,"_",years[j],".csv"),
               row.names = F)
-    
+
     # Rushing and Receiving
     rushing_standard <- html_content |>
       html_node("#rushing_standard") |>
@@ -46,12 +46,12 @@ for (i in 1:length(teams)) {
       clean_names() |>
       filter(rk != "") |>
       mutate(Team = team, Year = years[j])
-    
+
     write.csv(rushing_standard,
-              paste0("rushing_standard/rushing_standard","_",team,"_",years[j],".csv"), 
+              paste0("rushing_standard/rushing_standard","_",team,"_",years[j],".csv"),
               row.names = F)
-    
-    # Kick and Punt Returns 
+
+    # Kick and Punt Returns
     kick_return_standard <- html_content |>
       html_node("#kick_return_standard") |>
       html_table(fill = TRUE) |>
@@ -59,71 +59,71 @@ for (i in 1:length(teams)) {
       clean_names() |>
       filter(rk != "") |>
       mutate(Team = team, Year = years[j])
-    
+
     write.csv(kick_return_standard,
-              paste0("kick_return_standard/kick_return_standard","_",team,"_",years[j],".csv"), 
+              paste0("kick_return_standard/kick_return_standard","_",team,"_",years[j],".csv"),
               row.names = F)
-    
+
     # Defense and Fumble
     defense_standard <- html_content |>
       html_nodes(xpath = "//comment()") |>
       html_text() |>
       HTML()
-    
-    defense_standard_html <- read_html(defense_standard) |> 
+
+    defense_standard_html <- read_html(defense_standard) |>
       html_nodes('table.stats_table.sortable.per_toggler.soc#defense_standard') |>
       html_table()
-    
-    defense_standard_final <- defense_standard_html[[1]] |>  
+
+    defense_standard_final <- defense_standard_html[[1]] |>
       row_to_names(row_number = 1) |>
       clean_names() |>
       filter(rk != "") |>
       mutate(Team = team, Year = years[j])
-    
+
     write.csv(defense_standard_final,
-              paste0("defense_standard/defense_standard","_",team,"_",years[j],".csv"), 
+              paste0("defense_standard/defense_standard","_",team,"_",years[j],".csv"),
               row.names = F)
-    
+
     # Kicking and Punting
     kicking_standard <- html_content |>
       html_nodes(xpath = "//comment()") |>
       html_text() |>
       HTML()
-    
-    kicking_standard_html <- read_html(kicking_standard) |> 
+
+    kicking_standard_html <- read_html(kicking_standard) |>
       html_nodes('table.stats_table.sortable.per_toggler.soc#kicking_standard') |>
       html_table()
-    
-    kicking_standard_final <- kicking_standard_html[[1]] |>  
+
+    kicking_standard_final <- kicking_standard_html[[1]] |>
       row_to_names(row_number = 1) |>
       clean_names() |>
       filter(rk != "") |>
       mutate(Team = team, Year = years[j])
-    
+
     write.csv(kicking_standard_final,
-              paste0("kicking_standard/kicking_standard","_",team,"_",years[j],".csv"), 
+              paste0("kicking_standard/kicking_standard","_",team,"_",years[j],".csv"),
               row.names = F)
-    
+
     # Scoring
     scoring_standard <- html_content |>
       html_nodes(xpath = "//comment()") |>
       html_text() |>
       HTML()
-    
-    scoring_standard_html <- read_html(scoring_standard) |> 
+
+    scoring_standard_html <- read_html(scoring_standard) |>
       html_nodes('table.stats_table.sortable.per_toggler.soc#scoring_standard') |>
       html_table()
-    
-    scoring_standard_final <- scoring_standard_html[[1]] |>  
+
+    scoring_standard_final <- scoring_standard_html[[1]] |>
       row_to_names(row_number = 1) |>
       clean_names() |>
       filter(rk != "") |>
       mutate(Team = team, Year = years[j])
-    
+
     write.csv(scoring_standard_final,
-              paste0("scoring_standard/scoring_standard","_",team,"_",years[j],".csv"), 
+              paste0("scoring_standard/scoring_standard","_",team,"_",years[j],".csv"),
               row.names = F)
-    
+
     Sys.sleep(5)
     }, error = function(e) {
       return(NULL)
