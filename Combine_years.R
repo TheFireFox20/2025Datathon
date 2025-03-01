770daac11b008d88c84b27fcae579085228a1b8f
TheFireFox20 <jamesgphifer@gmail.com>
2025-03-01 07:50:01 -0700

    csv push

--- Combine_years.R
+++ Combine_years.R
@@ -1,5 +1,5 @@
 # Set the directory containing the CSV files
-directory <- "passing_standard"
+directory <- "scoring_standard"
 
 # List all CSV files in the directory
 files <- list.files(directory, pattern = "\\.csv$", full.names = TRUE)
@@ -16,12 +16,13 @@ combine_files <- function(file_list) {
   return(combined_data)
 }
 
-# Combine files for each year
+# Combine files for each yea
 combined_2021 <- combine_files(files_2021)
 combined_2022 <- combine_files(files_2022)
 combined_2023 <- combine_files(files_2023)
 
 # Write combined data to new CSV files
-write.csv(combined_2021, file.path("fullyearstats", "passing_2021.csv"), row.names = FALSE)
-write.csv(combined_2022, file.path("fullyearstats", "passing_2022.csv"), row.names = FALSE)
-write.csv(combined_2023, file.path("fullyearstats", "passing_2023.csv"), row.names = FALSE)
+write.csv(combined_2021, file.path("fullyearstats", "scoring_2021.csv"), row.names = FALSE)
+write.csv(combined_2022, file.path("fullyearstats", "scoring_2022.csv"), row.names = FALSE)
+write.csv(combined_2023, file.path("fullyearstats", "scoring_2023.csv"), row.names = FALSE)
+
