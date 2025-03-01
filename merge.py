218de90f0ad718f96fbb69d4daad3577d440231d
mnymns <loganmons01@gmail.com>
2025-03-01 11:26:13 -0700

    data done

--- merge.py
+++ merge.py
@@ -6,7 +6,9 @@ df_merged = pd.read_csv('merged.csv')
 df_portal = pd.read_csv('PortalData_2023.csv')
 
 # add null value for all 'Transfer' in merged to be adjusted later
-df_merged['Transfer'] = pd.NA
+df_merged['Transfer'] = False
+
+# df_final = pd.merge(df_merged,df_portal, left_on=['player', 'Team'],right_on=['Player', 'Team_New'],how = "left")
 
 df_final = df_merged.merge(
     df_portal[['Player', 'Team_New', 'Transfer']],  # Only keep necessary columns
@@ -15,13 +17,14 @@ df_final = df_merged.merge(
     how='left'
 )
 
-df_final.drop(columns=['Player', 'Team_New'], inplace=True, errors='ignore')
+df_final['transfer'] = df_final['Transfer_y'].apply(lambda x: x if pd.notna(x) else False)
+df_final.drop(columns=['Player', 'Team_New', 'Transfer_x', 'Player_y', 'Transfer_y'], inplace=True, errors='ignore')
 
 output = 'final.csv'
 
-# df_final.to_csv(output, index=False)
+df_final.to_csv(output, index=False)
 
-transfer_counts = df_final['Transfer_y'].value_counts(dropna=False)
+transfer_counts = df_final['transfer'].value_counts(dropna=False)
 
 num_true = transfer_counts.get(True, 0)
 num_false = transfer_counts.get(False, 0)
